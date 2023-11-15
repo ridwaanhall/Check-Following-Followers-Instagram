17f8306d6eb6c40d16022e577872af259a155601
ridwaanhall <erbyl14@gmail.com>
2023-11-15 17:49:30 +0700

    Make code in anpother file

--- Process/02_more_data_followers.py
+++ Process/02_more_data_followers.py
@@ -1,27 +1,27 @@
 from bs4 import BeautifulSoup
 
-html_content = """
-
-"""
+# Read HTML content from the file
+with open('code_followers.txt', 'r', encoding='utf-8') as file:
+    html_content = file.read()
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Mengekstrak informasi pengguna dari HTML
+# extract information user from HTML
 user_info_divs = soup.find_all('div', class_='x1dm5mii')
 
 for user_info_div in user_info_divs:
-    # Mendapatkan URL profil
+    # find url profile picture
     img_tag = user_info_div.find('img', draggable='false')
     if img_tag:
         profile_url = img_tag.get('src')
     else:
         profile_url = "Profile URL not found"
 
-    # Mendapatkan nama pengguna
+    # find username
     username_tag = user_info_div.find('span', class_='_ap3a')
     username = username_tag.get_text() if username_tag else "Username not found"
 
-    # Mendapatkan nama lengkap
+    # find full name
     full_name_tag = user_info_div.find('span', class_='x1lliihq')
     full_name = full_name_tag.text if full_name_tag else "Full name not found"
 
