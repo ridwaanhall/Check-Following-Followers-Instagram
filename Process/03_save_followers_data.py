29e92e49293de0e895b4c7d0692c8ca12feb094f
ridwaanhall <erbyl14@gmail.com>
2023-11-15 18:18:02 +0700

    change output to txt and csv

--- Process/03_save_followers_data.py
+++ Process/03_save_followers_data.py
@@ -1,6 +1,5 @@
 import os
 import csv
-import openpyxl
 from bs4 import BeautifulSoup
 
 # Get the current working directory
@@ -19,64 +18,44 @@ if os.path.exists(file_path):
     # Extract information about users from HTML
     user_info_divs = soup.find_all('div', class_='x1dm5mii')
 
-    # Write to CSV file
+    # Open a new CSV file for writing
     csv_file_path = os.path.join(current_dir, 'followers_data.csv')
     with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.writer(csv_file)
-        
+
         # Write header row to CSV file
         csv_writer.writerow(['Username', 'name', 'profile_url'])
 
-        for user_info_div in user_info_divs:
-            # Find URL profile picture
-            img_tag = user_info_div.find('img', draggable='false')
-            if img_tag:
-                profile_url = img_tag.get('src')
-            else:
-                profile_url = "Profile URL not found"
+        # Open a new text file for writing
+        txt_file_path = os.path.join(current_dir, 'followers_data.txt')
+        with open(txt_file_path, 'w', encoding='utf-8') as txt_file:
 
-            # Find username
-            username_tag = user_info_div.find('span', class_='_ap3a')
-            username = username_tag.get_text() if username_tag else "Username not found"
+            for user_info_div in user_info_divs:
+                # Find URL profile picture
+                img_tag = user_info_div.find('img', draggable='false')
+                if img_tag:
+                    profile_url = img_tag.get('src')
+                else:
+                    profile_url = "Profile URL not found"
 
-            # Find full name
-            full_name_tag = user_info_div.find('span', class_='x1lliihq')
-            full_name = full_name_tag.text if full_name_tag else "Full name not found"
+                # Find username
+                username_tag = user_info_div.find('span', class_='_ap3a')
+                username = username_tag.get_text() if username_tag else "Username not found"
 
-            # Write the information to the CSV file
-            csv_writer.writerow([username, full_name, profile_url])
+                # Find full name
+                full_name_tag = user_info_div.find('span', class_='x1lliihq')
+                full_name = full_name_tag.text if full_name_tag else "Full name not found"
 
-            # Print the information to the console
-            print(f"Username: {username}")
-            print(f"Full Name: {full_name}")
-            print(f"Profile URL: {profile_url}")
-            print("------")
+                # Write the information to the CSV file
+                csv_writer.writerow([username, full_name, profile_url])
 
-    print(f"Output saved to followers_data.csv")
+                # Write the information to the text file
+                txt_file.write(f"Username: {username}\n")
+                txt_file.write(f"Full Name: {full_name}\n")
+                txt_file.write(f"Profile URL: {profile_url}\n")
+                txt_file.write("------\n")
 
-    # Write to Excel file (xlsx)
-    excel_file_path = os.path.join(current_dir, 'followers_data.xlsx')
-    workbook = openpyxl.Workbook()
-    sheet = workbook.active
-
-    # Write header row to Excel file
-    sheet.append(['Username', 'name', 'profile_url'])
-
-    for user_info_div in user_info_divs:
-        img_tag = user_info_div.find('img', draggable='false')
-        profile_url = img_tag.get('src') if img_tag else "Profile URL not found"
-
-        username_tag = user_info_div.find('span', class_='_ap3a')
-        username = username_tag.get_text() if username_tag else "Username not found"
-
-        full_name_tag = user_info_div.find('span', class_='x1lliihq')
-        full_name = full_name_tag.text if full_name_tag else "Full name not found"
-
-        # Write the information to the Excel file
-        sheet.append([username, full_name, profile_url])
-
-    workbook.save(excel_file_path)
-    print(f"Output saved to followers_data.xlsx")
+        print(f"Output saved to followers_data.csv and followers_data.txt")
 
 else:
     print(f"File not found: {file_path}")
