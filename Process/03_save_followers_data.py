4d61b8a645c16e1777635a1d7e59edb842b15b24
ridwaanhall <erbyl14@gmail.com>
2023-11-15 18:15:17 +0700

    make to xlsx

--- Process/03_save_followers_data.py
+++ Process/03_save_followers_data.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import openpyxl
 from bs4 import BeautifulSoup
 
 # Get the current working directory
@@ -18,7 +19,7 @@ if os.path.exists(file_path):
     # Extract information about users from HTML
     user_info_divs = soup.find_all('div', class_='x1dm5mii')
 
-    # Open a new CSV file for writing
+    # Write to CSV file
     csv_file_path = os.path.join(current_dir, 'followers_data.csv')
     with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.writer(csv_file)
@@ -52,6 +53,30 @@ if os.path.exists(file_path):
             print("------")
 
     print(f"Output saved to followers_data.csv")
+
+    # Write to Excel file (xlsx)
+    excel_file_path = os.path.join(current_dir, 'followers_data.xlsx')
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+
+    # Write header row to Excel file
+    sheet.append(['Username', 'name', 'profile_url'])
+
+    for user_info_div in user_info_divs:
+        img_tag = user_info_div.find('img', draggable='false')
+        profile_url = img_tag.get('src') if img_tag else "Profile URL not found"
+
+        username_tag = user_info_div.find('span', class_='_ap3a')
+        username = username_tag.get_text() if username_tag else "Username not found"
+
+        full_name_tag = user_info_div.find('span', class_='x1lliihq')
+        full_name = full_name_tag.text if full_name_tag else "Full name not found"
+
+        # Write the information to the Excel file
+        sheet.append([username, full_name, profile_url])
+
+    workbook.save(excel_file_path)
+    print(f"Output saved to followers_data.xlsx")
 
 else:
     print(f"File not found: {file_path}")
