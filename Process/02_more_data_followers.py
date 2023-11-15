27c79486f7f61665f2da268d4411240232c1a96f
ridwaanhall <erbyl14@gmail.com>
2023-11-15 17:56:05 +0700

    FIx error

--- Process/02_more_data_followers.py
+++ Process/02_more_data_followers.py
@@ -1,32 +1,42 @@
+import os
 from bs4 import BeautifulSoup
 
-# Read HTML content from the file
-file_path = 'html_content.txt'
-with open(file_path, 'r', encoding='utf-8') as file:
-    html_content = file.read()
+# Get the current working directory
+current_dir = os.getcwd()
 
-soup = BeautifulSoup(html_content, 'html.parser')
+# Define the path to the HTML file
+file_path = os.path.join(current_dir, 'Process', 'html_content.txt')
 
-# extract information user from HTML
-user_info_divs = soup.find_all('div', class_='x1dm5mii')
+# Check if the file exists
+if os.path.exists(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        html_content = file.read()
 
-for user_info_div in user_info_divs:
-    # find url profile picture
-    img_tag = user_info_div.find('img', draggable='false')
-    if img_tag:
-        profile_url = img_tag.get('src')
-    else:
-        profile_url = "Profile URL not found"
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-    # find username
-    username_tag = user_info_div.find('span', class_='_ap3a')
-    username = username_tag.get_text() if username_tag else "Username not found"
+    # extract information user from HTML
+    user_info_divs = soup.find_all('div', class_='x1dm5mii')
 
-    # find full name
-    full_name_tag = user_info_div.find('span', class_='x1lliihq')
-    full_name = full_name_tag.text if full_name_tag else "Full name not found"
+    for user_info_div in user_info_divs:
+        # find url profile picture
+        img_tag = user_info_div.find('img', draggable='false')
+        if img_tag:
+            profile_url = img_tag.get('src')
+        else:
+            profile_url = "Profile URL not found"
 
-    print(f"Username: {username}")
-    print(f"Full Name: {full_name}")
-    print(f"Profile URL: {profile_url}")
-    print("------")
+        # find username
+        username_tag = user_info_div.find('span', class_='_ap3a')
+        username = username_tag.get_text() if username_tag else "Username not found"
+
+        # find full name
+        full_name_tag = user_info_div.find('span', class_='x1lliihq')
+        full_name = full_name_tag.text if full_name_tag else "Full name not found"
+
+        print(f"Username: {username}")
+        print(f"Full Name: {full_name}")
+        print(f"Profile URL: {profile_url}")
+        print("------")
+
+else:
+    print(f"File not found: {file_path}")
