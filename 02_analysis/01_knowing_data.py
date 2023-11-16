69eba06767ecb00bd6d732ff9e1c81bc60141ab4
ridwaanhall <erbyl14@gmail.com>
2023-11-16 07:06:22 +0700

    rename Username to username

--- 02_analysis/01_knowing_data.py
+++ 02_analysis/01_knowing_data.py
@@ -5,8 +5,8 @@ df_followers = pd.read_csv('followers_data.csv')
 df_following = pd.read_csv('following_data.csv')
 
 # count the number of unique usernames
-followers_count = df_followers['Username'].nunique()
-following_count = df_following['Username'].nunique()
+followers_count = df_followers['username'].nunique()
+following_count = df_following['username'].nunique()
 
 print('followers and following count:')
 print(f"followers count: {followers_count}")
--- 02_analysis/02_count.py
+++ 02_analysis/02_count.py
@@ -5,8 +5,8 @@ df_followers = pd.read_csv('followers_data.csv')
 df_following = pd.read_csv('following_data.csv')
 
 # Get lists of followers and following
-followers_list = df_followers['Username'].tolist()
-following_list = df_following['Username'].tolist()
+followers_list = df_followers['username'].tolist()
+following_list = df_following['username'].tolist()
 
 # Find out who follows you
 followers_of_you = set(followers_list)
@@ -24,6 +24,7 @@ not_followed_back = followers_of_you - following_by_you
 follow_each_other = followers_of_you.intersection(following_by_you)
 
 # Print or use the results as needed
+print('this is count of users who:')
 print(f"Followers of you                       : {len(followers_of_you)}")
 print(f"Following by you                       : {len(following_by_you)}")
 print(f"You follow but don't follow back       : {len(not_following_back)}")
@@ -31,5 +32,5 @@ print(f"You are followed but don't follow back : {len(not_followed_back)}")
 print(f"Follow each other                      : {len(follow_each_other)}")
 
 # list of users who don't follow you back
-print('this is user who don\'t follow you back:')
+print('\nthis is user who don\'t follow you back:')
 print(f"{not_following_back}")
